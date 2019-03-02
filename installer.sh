1184cf18e3e8ed2a48a30e6d1aad1a47c3d95f7c
OT <imot@protonmail.com>
2019-03-02 17:36:18 +0400

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 
-# Ensure we are being ran as root
+# Kali Anonsurf installer script
+
+# original source: https://github.com/Und3rf10w/kali-anonsurf By: Und3rf10w
+# edited by -07 https://github.com/OTsector/
+
+
 if [ $(id -u) -ne 0 ]; then
 	echo "This script must be ran as root"
 	exit 1
 fi
-
-# For upgrades and sanity check, remove any existing i2p.list file
-rm -f /etc/apt/sources.list.d/i2p.list
-
-# Compile the i2p ppa
-echo "deb http://deb.i2p2.no/ unstable main" > /etc/apt/sources.list.d/i2p.list # Default config reads repos from sources.list.d
-wget https://geti2p.net/_static/i2p-debian-repo.key.asc -O /tmp/i2p-debian-repo.key.asc # Get the latest i2p repo pubkey
-apt-key add /tmp/i2p-debian-repo.key.asc # Import the key
-rm /tmp/i2p-debian-repo.key.asc # delete the temp key
-apt-get update # Update repos
-
-if [[ -n $(cat /etc/os-release |grep kali) ]]
-then
-	apt-get install libservlet3.0-java 
-	wget http://ftp.us.debian.org/debian/pool/main/j/jetty8/libjetty8-java_8.1.16-4_all.deb
-	dpkg -i libjetty8-java_8.1.16-4_all.deb # This should succeed without error
-	apt-get install libecj-java libgetopt-java libservlet3.0-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni #installs i2p and other dependencies
-	apt-get -f install # resolves anything else in a broken state
+kVer=$(uname -r); kVer=${kVer::3}
+if [[ $kVer != "4.9" ]]; then
+	echo "This script is not for your kernel version"
+	exit 1
 fi
+if [ ! -f /usr/bin/apt-get ]; then
+	echo "This script is for Debian based systems only"
+	exit 1
+fi
+curl -o i2p-debian-repo.key.asc https://geti2p.net/_static/i2p-debian-repo.key.asc
+apt-key add i2p-debian-repo.key.asc
+rm -rf ./i2p-debian-repo.key.asc
+echo -e "deb https://deb.i2p2.de/ stretch main\ndeb-src https://deb.i2p2.de/ stretch main" > /etc/apt/sources.list.d/i2p.list
+apt-get install libservlet3.0-java -y
+if [ ! -f "libjetty8-java_8.1.16-4_all.deb" ]; then
+	wget http://ftp.us.debian.org/debian/pool/main/j/jetty8/libjetty8-java_8.1.16-4_all.deb
+fi
+dpkg -i libjetty8-java_8.1.16-4_all.deb
+apt-get install libecj-java libgetopt-java libservlet3.0-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni
+apt-get -f install
 
-apt-get install -y i2p-keyring #this will ensure you get updates to the repository's GPG key
-apt-get install -y secure-delete tor i2p # install dependencies, just in case
+apt-get install -y i2p-keyring
+apt-get install -y secure-delete tor i2p
 
-# Configure and install the .deb
-dpkg-deb -b kali-anonsurf-deb-src/ kali-anonsurf.deb # Build the deb package
-dpkg -i kali-anonsurf.deb || (apt-get -f install && dpkg -i kali-anonsurf.deb) # this will automatically install the required packages
+dpkg-deb -b kali-anonsurf-deb-src/ kali-anonsurf.deb
+dpkg -i kali-anonsurf.deb || (apt-get -f install && dpkg -i kali-anonsurf.deb)
 
 exit 0
+
+
